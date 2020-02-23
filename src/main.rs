b242fa8029780837f64202aa2a6ebcbc28aff298
Andrew Plaza <aplaza@liquidthink.net>
2020-02-23 22:13:02 +0100

    make a simple cli app

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,55 @@
-use subxt::{
-    balances::{self, AccountData, Balances},
-    contracts::Contracts,
-    system::{self, System},
-    Client,
-    DefaultNodeRuntime as Runtime,
-};
-use sp_keyring::AccountKeyring;
-use sp_runtime::generic::SignedBlock;
-use std::fs::File;
-use std::io::prelude::*;
 use codec::Encode;
+use std::{
+    fs::OpenOptions,
+    io::prelude::*,
+    path::{Path, PathBuf},
+};
+use subxt::{Client, DefaultNodeRuntime as Runtime};
+mod cli;
 
 fn main() {
+    let conf = cli::parse_args();
+
     async_std::task::block_on(async move {
-
-        let signer = AccountKeyring::Alice.pair();
-
-        let dest = AccountKeyring::Bob.to_account_id();
-
         let cli: Client<Runtime, _> = subxt::ClientBuilder::<Runtime>::new()
             .build()
-            .await.unwrap();
+            .await
+            .unwrap();
 
         let version = cli.version();
         let spec = version.spec_version;
         println!("SPEC: {}", spec);
 
-        let block = cli.block::<primitives::H256>(None).await.unwrap();
+        let block = cli.block::<primitives::H256>(conf.hash).await.unwrap();
         let block_num = block.as_ref().unwrap().block.header.number;
-        block.unwrap()
-             .block
-             .extrinsics
-             .iter()
+        block
+            .unwrap()
+            .block
+            .extrinsics
+            .iter()
             .enumerate()
-            .map(|(i, e)| {
-                 (i, e.encode())
-             })
-             .for_each(|(i, e)| {
-                 let file_name = format!("./EXTRINSIC_spec_{}_block_{}_index_{}.bin", spec, block_num, i);
-                 write_bytes_to_file(file_name, e);
-             });
+            .map(|(i, e)| (i, e.encode()))
+            .for_each(|(i, e)| {
+                let file_name = if let Some(o) = &conf.out {
+                    o.clone()
+                } else {
+                    PathBuf::from(format!(
+                        "./EXTRINSIC_spec_{}_block_{}_index_{}.bin",
+                        spec, block_num, i
+                    ))
+                };
+                write_bytes_to_file(file_name.as_path(), e);
+            });
     });
 }
 
-pub fn write_bytes_to_file<S: Into<String>>(path: S, bytes: Vec<u8>) {
-    let path: String = path.into();
-    let mut buffer = File::create(path.as_str()).unwrap();
+pub fn write_bytes_to_file<'a, P: Into<&'a Path>>(path: P, bytes: Vec<u8>) {
+    let path: &Path = path.into();
+    let mut buffer = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path.to_str().expect("Should be valid file path"))
+        .expect("File Path should be valid");
     buffer.write_all(&bytes[..]).unwrap();
+    buffer.write_all(b"\n").unwrap();
 }
